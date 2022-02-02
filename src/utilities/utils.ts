bb15ec80ca26ca9846d4b7c827e86a5a2d62ccd9
Cameron Robinson <cameron.k.robinson@gmail.com>
2022-02-02 12:36:41 -0800

    Added missing file header.

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,3 +1,12 @@
+/*
+ * Filename: multi-column-markdown/src/utilities/utils.ts
+ * Created Date: Tuesday, January 30th 2022, 4:02:19 pm
+ * Author: Cameron Robinson
+ * 
+ * Copyright (c) 2022 Cameron Robinson
+ */
+
+
 export function getUID(): string {
     return Math.random().toString(36).substring(2);
 }
