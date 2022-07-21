cd98bb439244f3d49aca145e3c2cc8db0d8a4ef2
Cameron Robinson <cameron@ckrobinson.net>
2022-07-20 20:47:17 -0700

    Added utility search for HTML node type.

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -17,3 +17,36 @@ export function getUID(length: number = 10): string {
     
     return UID;
 }
+
+/**
+ * BFS on the child nodes of the passed element searching for the first instance of the
+ * node type passed. Returning the element found or null if none found.
+ * 
+ * @param root 
+ * @param nodeTypeName
+ * @returns 
+ */
+export function searchChildrenForNodeType(root: HTMLElement, nodeTypeName: string): HTMLElement | null {
+
+    nodeTypeName = nodeTypeName.toLowerCase();
+    
+    let queue: HTMLElement[] = [root]
+    while(queue.length > 0){
+        
+        for(let i = 0; i < queue.length; i++) {
+            
+            let node = queue.shift()
+
+            let nodeName = node.nodeName.toLowerCase()
+            if(nodeName === nodeTypeName) {
+                return node as HTMLElement;
+            }
+
+            for(let i = 0; i < node.children.length; i++) {
+                queue.push(node.children[i] as HTMLElement)
+            }
+        }
+    }
+
+    return null;
+}
