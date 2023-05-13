1b64c2804c694f14c41fd2c954cf19e2327bea45
Cameron Robinson <cameron@ckrobinson.net>
2023-05-12 17:01:28 -0700

    Added function to convert css sizing to px.

--- src/utilities/interfaces.ts
+++ src/utilities/interfaces.ts
@@ -4,8 +4,10 @@
  *                                                                             *
  * Copyright (c) 2023 Cameron Robinson                                         *
  */
+import { pixels } from '@pacote/pixels'
 
 export class HTMLSizing {
+
     private _sizeValue: number = 0;
     public get sizeValue(): number {
         return this._sizeValue;
@@ -30,11 +32,113 @@ export class HTMLSizing {
         return `${this.sizeValue}${this.sizeUnits}`;
     }
 
+    convertToPX(parentElement: HTMLElement) {
+        if(this.sizeUnits === "px") {
+            return this;
+        }
+
+        switch(this.sizeUnits) {
+            case "cm":
+            case "mm":
+            case "in":
+            case "pt":
+            case "pc":
+                let absUnitsResult = pixels(`${this.toString()}`);
+                console.debug(`Converting ${this.sizeValue}${this.sizeUnits} to ${absUnitsResult}px`);
+                return new HTMLSizing(absUnitsResult, "px");
+            case "vw":
+            case "vh":
+                return handleViewportSizing();
+            case "em":
+                return getFontSizeFromEl();
+            case "ch":
+                let ch = createEl("p", {
+                    "attr": { "style": "width: 1ch;" }
+                })
+                return getSizeFromStyleWidth(ch);
+
+            case "ex":
+                let ex = createEl("p", {
+                    "attr": { "style": "width: 1ex;" }
+                })
+                return getSizeFromStyleWidth(ex);
+        }
+
+        function getSizeFromStyleWidth(el: HTMLParagraphElement) {
+
+            const DEFAULT_SIZE = 16;
+            if(parentElement === null ||
+                parentElement === undefined) {
+                 
+                 return new HTMLSizing(this.sizeValue * DEFAULT_SIZE, "px");
+             }
+
+            parentElement.appendChild(el);
+            let exToPxResult = this.sizeValue * el.clientWidth;
+            if(el.clientWidth === 0) {
+                exToPxResult = this.sizeValue * DEFAULT_SIZE;
+            } 
+
+            console.debug(`Converting ${this.sizeValue}${this.sizeUnits} to ${exToPxResult}px`);
+            parentElement.removeChild(el);
+            return new HTMLSizing(exToPxResult, "px");
+        }
+
+        function getFontSizeFromEl() {
+
+            const DEFAULT_SIZE = 16;
+            let fontSize = DEFAULT_SIZE;
+            let emToPxResult = fontSize * this.sizeValue;
+
+            if(parentElement === null ||
+               parentElement === undefined) {
+                
+                return new HTMLSizing(emToPxResult, "px");
+            }
+
+            let sizing = HTMLSizing.parseToSizing(parentElement.getCssPropertyValue("font-size"));
+            if (sizing !== null) {
+                fontSize = sizing.sizeValue;
+                emToPxResult = fontSize * this.sizeValue;
+                console.debug(`Converting ${this.sizeValue}${this.sizeUnits} to ${emToPxResult}px`);
+            }
+
+            return new HTMLSizing(emToPxResult, "px");
+        }
+
+        function handleViewportSizing() {
+
+            let scale = this.sizeValue / 100;
+            if(parentElement === null || parentElement === undefined) {
+                console.warn("Found undefined root element. Using default client size. Result may not appear as intended.");
+                let defaultSizing = scale * 1200;
+                return new HTMLSizing(defaultSizing, "px");
+            }
+
+            let viewWidth = parentElement.clientWidth;
+            let viewHeight = parentElement.clientHeight;
+            switch(this.sizeUnits) {
+                case "vw":
+                    let wToPxResult = scale * viewWidth;
+                    return new HTMLSizing(wToPxResult, "px");
+                case "vh":
+                case "%":
+                    let hToPxResult = scale * viewHeight;
+                    return new HTMLSizing(hToPxResult, "px");
+
+            }
+        }
+    }
+
     public static create(): HTMLSizing {
         return new HTMLSizing(0, "px");
     }
 
     public static parseToSizing(sizingText: string): HTMLSizing | null {
+
+        if(sizingText === "") {
+            return null;
+        }
 
         let unitData = HTMLSizing.getLengthUnit(sizingText);
         if(unitData.isValid === true) {
