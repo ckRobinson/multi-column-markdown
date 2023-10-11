14639373011b464704eb45c6b2d186579104401d
Cameron Robinson <cameron@ckrobinson.net>
2023-10-10 20:52:56 -0700

    Began adding danger zone section to settings view.

--- src/settings/MultiColumnSettingsView.ts
+++ src/settings/MultiColumnSettingsView.ts
@@ -1,4 +1,4 @@
-import { App, Platform, PluginSettingTab, Setting } from "obsidian";
+import { App, ButtonComponent, Platform, PluginSettingTab, Setting } from "obsidian";
 import MultiColumnMarkdown from "src/main";
 
 export default class MultiColumnSettingsView extends PluginSettingTab {
@@ -24,5 +24,44 @@ export default class MultiColumnSettingsView extends PluginSettingTab {
                     }
                 ));
         }
+
+        this.containerEl.createEl("hr")
+        this.containerEl.createEl("h4", { text: "DANGER ZONE" });
+        const dangerZoneContainerEl = this.containerEl.createDiv();
+
+        let docFrag = new DocumentFragment()
+        docFrag.createDiv({}, div  => {
+            div.createEl("h5", {}, span => {
+                span.setAttr("style", "color: var(--text-accent); margin-bottom: 0px; margin-top: 3px;")
+                span.innerText = "WARNING:"
+            })
+            div.createSpan({}, span => {
+                span.setAttr("style", "color: var(--text-accent);")
+                span.innerText = "This action modifies all relavent note files and may lead to corrupt file text."
+            })
+            div.createEl("br")
+            div.createSpan({}, span => {
+                span.setAttr("style", "color: var(--text-accent);")
+                span.innerText = "No guarentee is given. Please make sure to back your vault up first."
+            })
+            div.createEl("br")
+            div.createSpan({}, span => {
+                span.innerText = "This may take a while for large vaults."
+            })
+        })
+        let modalDescriptionEl = createDiv({}, div => {
+            div.createSpan({text: "This action may corrupt vault data."})
+            div.createEl("br")
+            div.createSpan({text: "Please confirm you have backed up your vault."})
+        })
+        new Setting(dangerZoneContainerEl)
+            .setName("Update ALL depreciated Multi-Column syntax.")
+            .setDesc(docFrag)
+            .addButton((b) =>
+                b.setButtonText("Update Syntax").onClick(() => {
+                })
+            );
+    }
+}
     }
 }
