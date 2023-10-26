17c7c0a1b081ce09ba4e02323f680b0adc245ead
Cameron Robinson <cameron@ckrobinson.net>
2023-10-25 22:28:53 -0700

    Fixed nul reference error on element unload.

--- src/live_preview/mcm_livePreview_widget.ts
+++ src/live_preview/mcm_livePreview_widget.ts
@@ -47,11 +47,11 @@ async function clearCache(skipKey: string = "") {
         let val = livePreviewElementCache.get(key)
 
         let deltaTimeMS = Date.now() - val.timestamp
-        if(val.element.parentNode.parentNode === null && deltaTimeMS > CACHE_MAX_DELTA_TIME_MS) {
+        if((val.element.parentNode === null || val.element.parentNode.parentNode === null) && deltaTimeMS > CACHE_MAX_DELTA_TIME_MS) {
             console.debug(`cache delta: ${deltaTimeMS} > ${CACHE_MAX_DELTA_TIME_MS} or 2 minutes.`)
             livePreviewElementCache.delete(key)
         }
-        else if(val.element.parentNode.parentNode === null) {
+        else if(val.element.parentNode == null || val.element.parentNode.parentNode === null) {
             
             console.debug(`Element ${index} null but not removing from cache yet. \nElement file path: ${key.split(" : ")[0]} \nPath Elapsed time: ${Math.floor(deltaTimeMS / 1000)}`)
         }
