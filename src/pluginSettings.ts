07cf80eacbb3b3c52aef81bc512bec1a9a1298a9
Cameron Robinson <cameron@ckrobinson.net>
2023-12-02 16:50:20 -0800

    Added new static singleton for settings.

--- src/pluginSettings.ts
+++ src/pluginSettings.ts
@@ -1,8 +1,23 @@
 export interface MCM_Settings {
     renderOnMobile: boolean;
     autoLayoutBalanceIterations: number;
+    useLivePreviewCache: boolean
 }
 export const DEFAULT_SETTINGS: MCM_Settings = {
     renderOnMobile: true,
-    autoLayoutBalanceIterations: 5
+    autoLayoutBalanceIterations: 5,
+    useLivePreviewCache: false
 };
+
+export class MCM_SettingsManager {
+
+    private static local: MCM_SettingsManager | null = null;
+    public static shared(): MCM_SettingsManager {
+        if(MCM_SettingsManager.local === null) {
+            MCM_SettingsManager.local = new MCM_SettingsManager()
+        }
+        return MCM_SettingsManager.local;
+    }
+
+    public settings: MCM_Settings = DEFAULT_SETTINGS;
+}
